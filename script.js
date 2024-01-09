0227c8e0f42188c0141fc85eb74bccd217c6dad8
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-01-09 05:37:32 +0100

    Updated

--- script.js
+++ script.js
@@ -7,6 +7,3 @@ z = x + y;
 function nombreDeFuncion() {
     const nombreVariable = 'Variable dentro de la función'
 }
-
-// Los comentarios se empiezan a escribir después de presionar Ctrl + }
-// NÚMEROS
