c66779f84f8549fdb2192c0c1362a95132d3fa6c
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-01-10 19:25:41 +0100

    Updated

--- 01-tipos/02-primitivos.js
+++ 01-tipos/02-primitivos.js
@@ -3,9 +3,11 @@ let texto = 'texto';
 let verdadero = true;
 let falso = false;
 let noDefinido;
+let nulo = null;
 
 console.log(numero);
 console.log(texto);
 console.log(verdadero);
 console.log(falso);
 console.log(noDefinido);
+console.log(nulo);
---
+++ 01-tipos/03-constantes.js
@@ -0,0 +1,4 @@
+let nombre = 'Hola Mundo!';
+nombre = 'Jose';
+
+console.log(nombre);
