4075ba99ccbdf7029195e21bf60160e90cdadbf6
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-01-10 19:22:06 +0100

    Updated

---
+++ 01-tipos/02-primitivos.js
@@ -0,0 +1,11 @@
+let numero = 1;
+let texto = 'texto';
+let verdadero = true;
+let falso = false;
+let noDefinido;
+
+console.log(numero);
+console.log(texto);
+console.log(verdadero);
+console.log(falso);
+console.log(noDefinido);
