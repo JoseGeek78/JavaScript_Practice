5d848a8c85ae566999b62e4a1f0478684d083ff5
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-01-11 19:39:14 +0100

    Updated

--- 01-tipos/06-objetos.js
+++ 01-tipos/06-objetos.js
@@ -2,3 +2,19 @@
 let nombre = 'Chihiro';
 let anime = 'El Viaje de Chihiro';
 let edad = 16;
+
+let personaje = {
+    nombre: 'Chihiro',
+    anime:'El Viaje de Chihiro',
+    edad: 16,
+};
+console.log(personaje);
+console.log(personaje.anime);
+console.log(personaje['nombre']);
+
+personaje.edad = 10;
+console.log(personaje.edad);
+
+delete personaje.edad;
+
+console.log(personaje);
