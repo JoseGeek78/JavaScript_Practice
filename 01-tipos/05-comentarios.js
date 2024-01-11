3fd8cf1ed41003d40336145eb2c5013eb29b7b53
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-01-11 18:16:16 +0100

    Updated

---
+++ 01-tipos/05-comentarios.js
@@ -0,0 +1,12 @@
+// nnwnvwlvw wvnwvnlw
+/**
+ * Y así
+ * se puede escribir múltiples
+ * líneas de comentarios
+ * 
+ * 
+ */
+
+let numero = 45;  // Esto sería un comentario
+
+console.log(numero);
