0974037a69d690079023c999b4ab693c7137f48a
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-01-16 20:03:03 +0100

    Updated

---
+++ 02-operadores/01-aritmeticos.js
@@ -0,0 +1,22 @@
+let a = 5;
+let b = 7;
+
+console.log(a + b, 'suma'); // suma
+console.log(a - b, 'resta'); // resta
+console.log(a * b, 'multiplicación'); // multiplicación
+console.log(a / b, 'división'); // división
+console.log(a % b, 'módulo o residuo'); //módulo o residuo: restante de un división
+console.log(a ** b, 'potencia'); // potencia
+
+
+var numero = 7;
+if (numero % 2 === 0) {
+    console.log(numero + " es par");
+} else {
+    console.log(numero + " es impar");
+}
+
+var dividendo = 10;
+var divisor = 4;
+var residuo = dividendo % divisor;
+console.log("El residuo de dividir " + dividendo + " entre " + divisor + " es " + residuo);
