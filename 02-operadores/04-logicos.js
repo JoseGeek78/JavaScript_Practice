1e96744fd533fd9fe4307b899d9ce231672216c7
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-01-17 18:49:48 +0100

    Updated

---
+++ 02-operadores/04-logicos.js
@@ -0,0 +1,18 @@
+// AND, OR, NOT
+
+// AND && 
+
+// Netflix: mayor de edad y suscriptor
+let mayor = false; // Prueba a cambiar a true
+let suscrito = true;
+// console.log(true && true);
+// console.log(false && true);
+console.log('operador and', mayor && suscrito);
+
+// OR ||
+console.log('operador and', mayor || suscrito);
+
+// NOT !
+console.log('operador and', !mayor);
+
+let catalogoInfantil = !mayor;
