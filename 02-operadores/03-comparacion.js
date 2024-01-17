a66a3a6cadae777f8e9bf4c22c1e91ac5b0a9f2c
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-01-17 18:37:08 +0100

    Updated

--- 02-operadores/03-comparacion.js
+++ 02-operadores/03-comparacion.js
@@ -1,2 +1,16 @@
 let a = 10;
 console.log(a > 5);
+console.log(a >= 5);
+
+let b = 6;
+console.log(b > 6);
+console.log(b >= 6);
+
+let c = 7;
+console.log(c < 7);
+console.log(c <= 7);
+
+console.log(c == 7);
+console.log(c != 7);
+console.log(c === '7');
+console.log(c !== 7);
