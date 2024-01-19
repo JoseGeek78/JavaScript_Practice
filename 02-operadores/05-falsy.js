bccf023ca61591bd68951c06c03c38750cbd0b6a
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-01-19 06:08:36 +0100

    Updated

--- 02-operadores/05-falsy.js
+++ 02-operadores/05-falsy.js
@@ -13,3 +13,14 @@ let nombre = 'Jose';
 let username = nombre || 'Anónimo';
 console.log(username);
 
+function fn1() {
+    console.log('Soy la función 1');
+    return false;
+}
+
+function fn2() {
+    console.log('Soy la función 2');
+    return true;
+}
+
+let x = fn1() && fn2();
